1cb20c0c83962df87a68c8500dac76338d4f6dd3
Jai Joshi <83172501+jxjoshi@users.noreply.github.com>
2021-06-26 10:25:23 -0500

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,30 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.image = loadImage("block.png")
+      this.Visibility = 225;
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      console.log(this.body.speed);
+      if(this.body.speed < 3){
+      var pos= this.body.position;
+      imageMode(CENTER);
+      image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+      }
+      else{
+        World.remove(world, this.body);
+        push();
+        this.Visibility = this.Visibility - 5;
+        tint(255, this.Visibility);
+        image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+        pop();
+      }
+    }
+}
